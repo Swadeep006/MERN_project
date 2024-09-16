e21d9f752da9ef2ba5685ae576e2fcfdb2fdf99e
Swadeep Dhondi <dhondiswadeepdsp6@gmail.com>
2024-09-16 12:34:46 +0530

    color,Theme,DarkMode and Setup[1]

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { version } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -18,10 +18,10 @@ import {
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = { key: "Root", storage, version: 1 };
-persistReducer = persistReducer(persistConfig, authReducer);
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
